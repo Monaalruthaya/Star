422ca9a3930620326d69594f5d94edb5c1ca6df0
Monaalruthaya <monaalruthaya@outlook.com>
2024-12-12 16:54:40 +0300

    Initial Commit

---
+++ StarTests/StarTests.swift
@@ -0,0 +1,17 @@
+//
+//  StarTests.swift
+//  StarTests
+//
+//  Created by mona alruthaya on 11/06/1446 AH.
+//
+
+import Testing
+@testable import Star
+
+struct StarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
